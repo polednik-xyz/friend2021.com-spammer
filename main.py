445848927a84a5af41d1f4413e6f0f9d4a852cfd
Daniel <110359376+polednik-xyz@users.noreply.github.com>
2022-08-01 01:38:53 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+
+import requests
+url = "https://api.truefalse.site/d20/addentry"
+
+data = {
+"key": "1",
+"name": "", ## username
+"name1": "", ## name of the quizer
+"quizid": "", ##quiz id in url
+"score": "20", ## quiz score cannot go above 20
+}
+while True:
+    r = requests.post(url, data=data).text
+    print("sent! if no response then patched or bad quizid")
+    print(r)
+
